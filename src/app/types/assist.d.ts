42b8066bb7873c74460deaea0a00f7caa071fef4
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-11-12 15:22:31 -0300

    :label: Assist type

---
+++ src/app/types/assist.d.ts
@@ -0,0 +1,32 @@
+export interface Assist {
+  id: number
+  player_name: string
+  age: number
+  games: number
+  games_started: number
+  minutes_played: number
+  field_goals: number
+  field_attempts: number
+  field_percent: string
+  three_fg: number
+  three_attempts: number
+  three_percent: string
+  two_fg: number
+  two_attempts: number
+  two_percent: string
+  effect_fg_percent: string
+  ft: number
+  fta: number
+  ft_percent: string
+  ORB: number
+  DRB: number
+  TRB: number
+  AST: number
+  STL: number
+  BLK: number
+  TOV: number
+  PF: number
+  PTS: number
+  team: string
+  season: number
+}
