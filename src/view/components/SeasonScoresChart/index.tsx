a48af465cf28dc9e1eee5f537b7ffce76f10869c
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-11-13 11:48:36 -0300

    :bug: fix: chart background gradient

--- src/view/components/SeasonScoresChart/index.tsx
+++ src/view/components/SeasonScoresChart/index.tsx
@@ -24,12 +24,22 @@ ChartJS.register(
   Filler
 );
 
+interface ChartContext {
+  chart: {
+    ctx: CanvasRenderingContext2D;
+    chartArea: {
+      bottom: number;
+      top: number;
+    };
+  };
+}
+
 interface SeasonScoresChartProps {
   seasons: Player[] | undefined
 }
 
 export function SeasonScoresChart({ seasons }: SeasonScoresChartProps) {
-  function createBackgroundGradient(context, color: string) {
+  function createBackgroundGradient(context: ChartContext, color: string) {
     const chart = context.chart;
     const { ctx, chartArea } = chart;
     if (!chartArea) {
@@ -48,7 +58,7 @@ export function SeasonScoresChart({ seasons }: SeasonScoresChartProps) {
         label: 'Assists',
         data: seasons?.sort((a, b) => a.season - b.season).map(season => season.AST),
         borderColor: 'rgb(23, 64, 139)',
-        backgroundColor: (context) => createBackgroundGradient(context, '23, 64, 139'),
+        backgroundColor: (context: ChartContext) => createBackgroundGradient(context, '23, 64, 139') ?? 'rgba(23, 64, 139, 0.5)',
         fill: true,
         tension: 0.5
       },
@@ -56,7 +66,7 @@ export function SeasonScoresChart({ seasons }: SeasonScoresChartProps) {
         label: 'Points',
         data: seasons?.sort((a, b) => a.season - b.season).map(season => season.PTS),
         borderColor: 'rgb(201, 8, 42)',
-        backgroundColor: (context) => createBackgroundGradient(context, '201, 8, 42'),
+        backgroundColor: (context: ChartContext) => createBackgroundGradient(context, '201, 8, 42') ?? 'rgba(201, 8, 42, 0.5)',
         fill: true,
         tension: 0.5
       },
