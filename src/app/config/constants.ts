b1399961c696be70aa32e8650a121760955b4bcd
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-11-12 22:08:09 -0300

    chore: top 5 2023 season players pictures

---
+++ src/app/config/constants.ts
@@ -0,0 +1,28 @@
+import GiannisAntetokounmpo from '../../assets/images/giannis-antetokounmpo.jpg';
+import JaysonTatum from '../../assets/images/jayson-tatum.jpg';
+import JoelEmbiid from '../../assets/images/joel-embiid.jpg';
+import LukaDondic from '../../assets/images/luka-dondic.jpg';
+import ShaiGilgeousAlexander from '../../assets/images/shai-gilgeous-alexander.jpeg';
+
+export const playerPictures = [
+  {
+    name: 'Giannis Antetokounmpo',
+    picture: GiannisAntetokounmpo
+  },
+  {
+    name: 'Jayson Tatum',
+    picture: JaysonTatum
+  },
+  {
+    name: 'Joel Embiid',
+    picture: JoelEmbiid
+  },
+  {
+    name: 'Luka Dondic',
+    picture: LukaDondic
+  },
+  {
+    name: 'Shai Gilgeous-Alexander',
+    picture: ShaiGilgeousAlexander
+  },
+];
