f65aa879bee6500763b8c8f63da98be17ad11554
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-11-12 23:45:10 -0300

    :lipstick: changed style

--- src/view/components/Footer/index.tsx
+++ src/view/components/Footer/index.tsx
@@ -2,8 +2,8 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className='w-full h-[60px] border-t-[1px] border-nba-blue flex items-center justify-center'>
-      <p className='text-nba-blue'>&copy; {currentYear} Matheus Germano da Costa. Todos os direitos Reservados</p>
+    <footer className='w-full h-[60px] bg-zinc-100 border-t-[1px] border-zinc-300 flex items-center justify-center'>
+      <p className='text-zinc-700'>&copy; {currentYear} Matheus Germano da Costa. Todos os direitos Reservados</p>
     </footer>
   );
 }
